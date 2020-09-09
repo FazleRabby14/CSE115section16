d639d4b54f9086e44bbaf1a3567e8331995343d6
Fazle Rabby <km.fazla@northsouth.edu>
2020-09-09 08:09:28 +0600

    Week-7 part-2 Problems

---
+++ WEEK-7 PART-2 PROBLEM/PROBLEM-4/problem-4.c
@@ -0,0 +1,26 @@
+/* 4. Implement the following function which reverses a string without using
+library function.*/
+#include<stdio.h>
+#include<String.h>
+void reverse(char arr[]);
+
+int main(){
+    char str[20];
+    int n;
+    gets(str);
+    reverse(str);
+    return 0;
+}
+void reverse(char arr[]){
+    int n;
+    printf("Enter any number:");
+    scanf("%d",&n);
+    printf("String order:\n");
+    for(int i=0;i<n;i++){
+        printf("%c",arr[i]);
+    }
+    printf("\nReverse order:\n");
+    for(int i=n-1;i>=0;i--){
+        printf("%c",arr[i]);
+    }
+}
