43c568d984c100e5cf29d434a3016bb731c74050
Fazle Rabby <km.fazla@northsouth.edu>
2020-09-29 07:28:59 +0600

    Week-5 problems are done

---
+++ WEEK -5 PROBLEM/PROBLEM-1/problem-1.c
@@ -0,0 +1,20 @@
+/* Write a program that takes 5 integers as input, stores them in an array and prints
+them in the reverse order of the input.*/
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    int arr[5];
+    printf("Enter numbers of array:\n");
+    for(i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Reverse numbers of arrays are:\n");
+
+    for(i=4;i>=0;i--){
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}
